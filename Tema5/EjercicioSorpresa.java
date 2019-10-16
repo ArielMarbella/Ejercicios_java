5272599d33615062f214f1967db0e19308ca84e0
ArielMarbella <fpdawcampanillas@gmail.com>
2019-10-16 10:24:18 +0200

    actualizacion

---
+++ Tema5/EjercicioSorpresa.java
@@ -0,0 +1,25 @@
+/*
+
+ */
+
+
+public class EjercicioSorpresa {
+	
+	public static void main (String args[]) {
+    
+    System.out.print("por favor introduce la altura inicial del reloj de arena ");
+    int alturaIntroducida = Integer.parseInt(System.console().readLine());
+    System.out.print("introduzca un carácter con el que pintar la piramide ");
+    int caracterIntroducido = Integer.parseInt(System.console().readLine());
+    System.out.print("numero de veces que se va a dibujar");
+    int incremento = 0;
+    
+    if ((alturaIntroducida < 3)||(alturaIntroducida % 2 == 0)){
+      System.out.print("altura introducida es incorrecta");
+    }else{
+      while () 
+      
+      }
+	}
+}
+
