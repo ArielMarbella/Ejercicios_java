cb865ed235e6fe55673597bc3edfcef1250bfa88
ArielMarbella <fpdawcampanillas@gmail.com>
2019-10-19 17:41:14 +0200

    actualizacion

--- Tema5/Tema05Ejercicio16.java
+++ Tema5/Tema05Ejercicio16.java
@@ -9,7 +9,25 @@
 public class Tema05Ejercicio16 {
 	
 	public static void main (String args[]) {
-		
-	}
+   int numIntro;
+    boolean primo = true;
+    
+    System.out.println("Este programa te dice si el numero que introduzcas es primo o no");
+    
+    System.out.println("Introduce aqui el numero");
+    numIntro = Integer.parseInt(System.console().readLine());
+    
+    for (int i = 2; i < numIntro; i++){
+      if ((numIntro % i) == 0){
+        primo = false;
+      }
+    }
+    
+    if (primo) {
+      System.out.println ("El numero es primo");
+    } else {
+      System.out.println ("El numero no es primo");
+    }
+  }
 }
 
