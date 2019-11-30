86c039be3381ca91f1f4c62b106e2bf76a05ae27
ArielMarbella <fpdawcampanillas@gmail.com>
2019-11-30 14:04:23 +0100

    actualizacion

--- vueltaAjava/Tema05/Tema05Ejer34.java
+++ vueltaAjava/Tema05/Tema05Ejer34.java
@@ -14,7 +14,71 @@ int donde sea necesario para admitir números largos.
 public class Tema05Ejer34 {
 	
 	public static void main (String args[]) {
-		
+    System.out.println("Introduce un número con pares e impares");
+    int numero1 = Integer.parseInt(System.console().readLine());
+    
+    System.out.println("Introduce un número con pares e impares");
+    int numero2 = Integer.parseInt(System.console().readLine());
+    
+    int volteado1 =0;
+    int volteado2 =0;
+    int n1 = numero1;
+    int n2 = numero2;
+
+    int longitud=0;
+    int longitud2 =0;
+    
+    int resultadoPar=0;
+    int resultadoImpar=0;
+    
+    int digito=0;
+
+    if (n1==0){
+      System.out.println("este número no vale");
+    }
+    if (n2==0){
+      System.out.println("este número no vale");
+    }
+    
+    while(n1>0&&n2>0){
+      
+      volteado1= (volteado1*10)+(n1%10);
+      n1=n1/10;
+      longitud++;
+      
+      volteado2= (volteado2*10)+(n2%10);
+      n2=n2/10;
+      longitud2++;
+    
+    }//while
+      
+    for(int i=0;i<longitud;i++) {
+
+      digito = volteado1%10;
+      
+      if (digito%2==0){
+        
+        resultadoPar= (resultadoPar*10) + digito;
+      }else{
+        resultadoImpar=(resultadoImpar*10)+digito;
+       }
+       
+      digito = volteado2%10;
+      
+      if (digito%2==0){
+        
+        resultadoPar= (resultadoPar*10) + digito;
+      }else{
+        resultadoImpar=(resultadoImpar*10)+digito;
+       }
+       
+       volteado1=volteado1/10;
+       volteado2=volteado2/10;
+    }
+    
+    System.out.println("El número formado por resultados pares es = " +   resultadoPar);
+    System.out.println("El número formado por resultados impares es = " + resultadoImpar);
+    
 	}
 }
 
