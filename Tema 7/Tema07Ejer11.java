d0c3f7ea58035a623263555035877b7974058fff
ArielMarbella <fpdawcampanillas@gmail.com>
2020-01-03 19:08:50 +0100

    actualizacion

--- Tema 7/Tema07Ejer11.java
+++ Tema 7/Tema07Ejer11.java
@@ -11,7 +11,65 @@
 public class Tema07Ejer11 {
 	
 	public static void main (String args[]) {
-		
+    
+     int[] num = new int[10];
+    int[] primo = new int[10];
+    int[] noPrimo = new int[10];
+    int i;
+    int j;
+    int primos = 0;
+    int noPrimos = 0;
+    boolean esPrimo = false;
+		System.out.println("introduce 10 números y serán ordenados");
+    
+    for (i=0;i< 10;i++) {
+      esPrimo = true;
+      num[i] = Integer.parseInt(System.console().readLine());
+      
+    for (j = 2; j < num[i]; j++) {
+      if (num[i] % j == 0) {
+      esPrimo = false;
+	    }
+    }
+      
+      if (esPrimo) {
+      primo[primos++] = num[i];
+      } else {
+      noPrimo[noPrimos++] = num[i];
+      }
+    }
+    
+    // Imprime array inicial
+    System.out.println("Array inicial: ");
+    System.out.print("Indice: ");
+    for (i = 0; i < 10; i++) {
+      System.out.printf("%5d", i);
+    }
+    System.out.print("\nValor:  ");
+    for (i = 0; i < 10; i++) {
+      System.out.printf("%5d", num[i]);
+    }
+    
+    // Introduce los números primos al princio del array final
+    for (i = 0; i < primos; i++) {
+      num[i] = primo[i];
+    }
+    // Introduce los no primos una vez se hayan introducido los primos
+    for (i = primos; i < 10; i++) {
+      num[i] = noPrimo[i - primos];
+    }
+    
+    // Imprime array final
+    System.out.println("\n\nArray final: ");
+    System.out.print("Indice: ");
+    for (i = 0; i < 10; i++) {
+      System.out.printf("%5d", i);
+    }
+    System.out.print("\nValor:  ");
+    for (i = 0; i < 10; i++) {
+      System.out.printf("%5d", num[i]);
+    }
+    
 	}
 }
 
